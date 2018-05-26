552ea8b2dc136a040845a73b7323970abcb355d7
Thiago Silva <thiagosb13@gmail.com>
2018-05-25 21:32:03 -0300

    Creating Project
    Created a spring boot web project

---
+++ src/main/java/com/thiagobezerra/bankslips/BankslipsApplication.java
@@ -0,0 +1,12 @@
+package com.thiagobezerra.bankslips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankslipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankslipsApplication.class, args);
+	}
+}
